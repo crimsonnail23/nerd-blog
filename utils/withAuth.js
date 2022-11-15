fa17311fe4e30a9af5a15af6d640cba790c656a6
night <yseddiq@gmail.com>
2022-11-15 18:14:34 -0500

    finished authentication check logic in utils/withAuth.js

--- utils/withAuth.js
+++ utils/withAuth.js
@@ -1,2 +1,12 @@
 //following code checks if you're login crenditials are authenticated, if not
 // it'll send you back to login.
+
+const withAuth = (req,res,next)=>{
+    if (!req.session.user_id){
+        res.redirect('/login');
+    }else{
+        next();
+    }
+};
+
+module.exports = withAuth;
