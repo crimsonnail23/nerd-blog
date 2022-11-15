498619b18c9cf0ae6baf15f19ec945b61d18b09c
night <yseddiq@gmail.com>
2022-11-15 17:39:09 -0500

    started the GET ALL endpoint in home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+
+// this is the '/' endpoint.
+router.get('/', (req,res)=>{
+    Post.findAll({
+
+    })
+})
