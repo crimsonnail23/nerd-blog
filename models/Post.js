225c127cfff974bacfd914fb08e8838bd6c53010
night <yseddiq@gmail.com>
2022-11-15 16:54:51 -0500

    started to define tables and columns for Post model

--- models/Post.js
+++ models/Post.js
@@ -1,2 +1,16 @@
 const {Model, DataTypes}= require('sequelize');
 const sequelize = require('../config/connection')
+
+//create post mode.
+class Post extends Model{};
+
+Post.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+    }
+)
