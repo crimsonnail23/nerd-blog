4c1910f11c3c569db9da0cc30b2e49778af7ec6d
night <yseddiq@gmail.com>
2022-11-15 16:22:16 -0500

    started work on User model properties

--- models/Comment.js
+++ models/Comment.js
@@ -8,6 +8,17 @@ Comment.init(
         id:{
             type: DataTypes.INTEGER,
             allowNull: false,
+            primaryKey: true,
+            autoIncrement: true 
+        },
+        commentBody:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            //following code will require a minimum number of characters for all comments.
+            validate: {
+
+                len:[5]
+            } 
         }
     }
 )
