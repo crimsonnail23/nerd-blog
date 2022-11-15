53074bd5d15b0f422789d54551db8ad826266e2b
night <yseddiq@gmail.com>
2022-11-15 15:49:44 -0500

    made progress on connecting to to database in config/connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,3 +2,8 @@
 const Sequelize= require('sequelize');
 
 require('dotenv').config();
+
+//create connection to database and pass in ENV variables.
+const sequelize= new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
+    
+})
