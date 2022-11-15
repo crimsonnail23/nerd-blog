326ee03d53ae2d7ee9eed224957dd8f682c53e51
night <yseddiq@gmail.com>
2022-11-15 17:58:51 -0500

    forgot to hit save for last commit

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -25,4 +25,16 @@ router.get('/', (req,res)=>{
             }
         ]
     })
-})
+    .then(dbPostData =>{
+        //pass single post into the homepage template.
+        const posts = dbPostData.map(post=>post.get({ plain: true}))
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
