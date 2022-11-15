1632468df7d5c533ae0c65ca4599661b0b980c9d
night <yseddiq@gmail.com>
2022-11-15 16:11:20 -0500

    started defining the properties of comment model and their datatypes

--- models/Comment.js
+++ models/Comment.js
@@ -1,2 +1,13 @@
 const { Model, DataTypes} = require('sequelize');
-const sequelize = require('../config/connection')
+const sequelize = require('../config/connection');
+
+class Comment extends Model{}
+
+Comment.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        }
+    }
+)
