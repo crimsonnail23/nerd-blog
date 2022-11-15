5982bfeb9ffb9f56e697fd961d58f2484cfa936b
night <yseddiq@gmail.com>
2022-11-15 17:57:59 -0500

    added GET ALL route in home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,6 +5,24 @@ const { Post, User, Comment } = require('../models');
 // this is the '/' endpoint.
 router.get('/', (req,res)=>{
     Post.findAll({
-
+        attributes:[
+            'id',
+            'post_url',
+            'title',
+            'created_at'
+        ], include:[
+            {
+                model: Comment,
+                attributes:['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include:{
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
     })
 })
