1a29a5537f482a54e58e7ca7287bf6ad2fe63648
night <yseddiq@gmail.com>
2022-11-15 18:35:18 -0500

    added comments to controllers/index.js, and finished the logic for the file

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,13 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
+//will add 'api', or 'dashboard' to the various endpoints.
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
+
+router.use((req,res)=>{
+    res.status(404).end();
+});
+
+module.exports = router;
