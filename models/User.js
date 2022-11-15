21afa7cf5b4ca8dc83d8f89d36b20f326b9eb334
night <yseddiq@gmail.com>
2022-11-15 16:26:50 -0500

    done adding the different tables and columns in user model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes}= require('sequelize');
+const sequelize = require('sequelize');
+
+class User extends Model{};
+
+User.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            //password must be at least 8 characters long.
+            validate: {
+                len: [8]
+            }
+        }    
+    },
+    
+)
