156e5b63031b6df832d5f61d1d50d14bb34ea141
night <yseddiq@gmail.com>
2022-11-16 08:08:01 -0500

    started work on Get All route for Posts.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,3 +2,10 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/withAuth');
 const { Post, User, Comment} = require('../../models');
+
+//GET ALL posts. route = api/posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        
+    })
+})
