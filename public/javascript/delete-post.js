7f5d5aa9aa0f23a2bddc14b33e3f27097b12c980
night <yseddiq@gmail.com>
2022-11-16 14:10:54 -0500

    finished work on front end logic for deleting a post in delete-post.js

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,5 +1,19 @@
 async function deleteHandler(event){
     event.preventDefault();
+
+    const id= window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1];
+    const response =await fetch(`/api/posts/${id}`,{
+        method: 'DELETE'
+    });
+    
+    //make a delete request, if it goes through then load dashboard.
+    // if the request fails then throw an alert.
+    if(response.ok){
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText)
+    }
 }
 
 document.querySelector('.delete-post-btn').addEventListener('click', deleteHandler);
