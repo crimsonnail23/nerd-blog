ad7b952829e5b36fddebfb5e6de4fc0c92314c31
night <yseddiq@gmail.com>
2022-11-16 09:54:52 -0500

    added sequelize session code to server.js

--- server.js
+++ server.js
@@ -7,13 +7,29 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
 
+const sess = {
+    secret: 'Superman is Clark Kent',
+    cooke: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
 
-//insert app.user(routes) here to turn on routes.
+//turn the routes on. 
+app.use(routes);
 
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => console.log(`listening to ${PORT}`));
