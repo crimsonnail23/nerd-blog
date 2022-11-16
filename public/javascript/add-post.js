8c4aa48ceadf152389e98198639111d3780ea2ec
night <yseddiq@gmail.com>
2022-11-16 14:17:05 -0500

    fixed typo

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,7 +1,7 @@
 async function newPostHandler(event){
     event.preventDefault();
     const title = document.querySelector('input[name="post-title"]').value;
-    const post_url = document.querySelector('input[name="post-url"]').value;
+    const post_link = document.querySelector('input[name="post-url"]').value;
 
     const response = await fetch('/api/posts', {
         method: 'POST',
