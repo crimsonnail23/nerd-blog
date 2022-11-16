295def222b69145f57829daae572adc09c12c589
night <yseddiq@gmail.com>
2022-11-16 14:39:34 -0500

    finished front end logic for logging out in logout.js

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,5 +1,16 @@
 async function logout(){
-    
+    const response = await fetch('api/users/logout',{
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+        }
+    });
+    //if logout was successful then go to home page.
+    if(response.ok){
+        document.location.replace('/');
+    } else {
+        alert(response.statusText)
+    }
 }
 
 document.querySelector('#logout').addEventListener('click', logout);
