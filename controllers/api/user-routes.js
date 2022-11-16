090af45d953819375e02d4a7dc189094a92e4876
night <yseddiq@gmail.com>
2022-11-16 07:40:56 -0500

    added GET for single user by ID in api/user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -15,4 +15,38 @@ router.get('/', (req,res)=>{
         });
 });
 
+//GET sinle user by id. Route= /api/users/:id
+router.get('/:id', (req,res)=>{
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where:{
+            id: req.params.id
+        },
+        include:[
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_link', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'commentBody', 'created_at'],
+                include:{
+                    model: Post,
+                    attibutes: ['title']
+                }
+            }
+        ]
+    })
+    .then(dbUserData=>{
+        if(!dbUserData){
+            res.status(404).json({ message: 'no user with that id, sorry'})
+        }
+        res.json(dbUserData)
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json(err);
+    })
+})    
+
 module.exports = router;
