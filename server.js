f06ca33a03f07b235e33bc4e8228fedc8ec9a4a7
night <yseddiq@gmail.com>
2022-11-16 09:50:25 -0500

    added import statements to server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
-//insert const routes here.
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
