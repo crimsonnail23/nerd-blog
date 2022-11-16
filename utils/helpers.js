2f6d52ba3f2e289f480c00b34abee6a97ab2837a
night <yseddiq@gmail.com>
2022-11-16 14:44:20 -0500

    finished format_date function in utils/helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,8 @@
 module.exports ={
     // following code will format date to be more legible.
     format_date: date=>{
-        
+        return `
+        ${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}
+        `
     }
 }
