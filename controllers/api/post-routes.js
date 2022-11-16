1d291dd56ba59310c3980d3cd07c7198980ea7b3
night <yseddiq@gmail.com>
2022-11-16 09:24:14 -0500

    moved over a update post route, and added delete post route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -89,28 +89,50 @@ router.post('/', withAuth, (req, res) => {
       });
   });
 
-  //update post. route= api/posts/:id need to be logged in for this to work.
-  router.put('/:id', withAuth, (req, res) => {
-    Post.update(
-      {
-        title: req.body.title
-      },
-      {
-        where: {
-          id: req.params.id
-        }
-      }
-    )
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-        res.json(dbPostData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+//update post. route= api/posts/:id need to be logged in for this to work.
+router.put('/:id', withAuth, (req, res) => {
+Post.update(
+    {
+    title: req.body.title
+    },
+    {
+    where: {
+        id: req.params.id
+    }
+    }
+)
+    .then(dbPostData => {
+    if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+    }
+    res.json(dbPostData);
+    })
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+console.log('id', req.params.id);
+Post.destroy({
+    where: {
+    id: req.params.id
+    }
+})
+    .then(dbPostData => {
+    if (!dbPostData) {
+        res.status(404).json({ message: 'no post found with this id' });
+        return;
+    }
+    res.json(dbPostData);
+    })
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
+});
+
+module.exports = router;
   
