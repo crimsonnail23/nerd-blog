d49bbef4848caf3b4f386d8bc32b16a925e2c846
night <yseddiq@gmail.com>
2022-11-16 07:32:10 -0500

    added GET ALL route for Users.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,18 @@
 const router = require('express').Router();
 const withAuth = require('../../utils/auth');
 const { User, Post, Comment } = require('../../models');
+
+//get all users. route= /api/users
+router.get('/', (req,res)=>{
+    //use findAll method with User model.
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err =>{
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
