e135b1780b0f90d2bbed5def0f2ef0b15c1a3f9e
night <yseddiq@gmail.com>
2022-11-16 07:54:33 -0500

    added login route for Users in user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -68,4 +68,33 @@ router.post('/', withAuth, (req,res)=>{
     })
 });
 
+//login route = api/login
+router.post('/login', (req,res)=>{
+    User.findOne({
+        where:{
+            email: req.body.email
+        }
+    }).then(dbUserData=>{
+        if(!dbUserData){
+            res.status(400).json({ message: 'no user with that email address'});
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if(!validPassword){
+            res.status(400).json ({ message: 'incorrect password' });
+            return;
+        }
+        req.session.save(()=>{
+            //declare session variables.
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username,
+            req.session.loggedIn= true;
+
+            res.json({ user: dbUserData, message: 'you are now logged in' });
+        });
+    });
+});
+
+
 module.exports = router;
