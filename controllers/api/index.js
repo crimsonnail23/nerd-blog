1f7f83b00078eefff410f1fd9276ce36b4d5037f
night <yseddiq@gmail.com>
2022-11-16 08:06:03 -0500

    added comment to controllers/api/index.js and added import statements to post-routes.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 
+// will add '/api/users to the appropriate endpoints.
 router.use('/users', userRoutes);
 
 module.exports = router;
