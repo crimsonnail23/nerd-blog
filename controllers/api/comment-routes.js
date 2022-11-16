07080ff09185a3cb16893f824b1a4dcd7b832995
night <yseddiq@gmail.com>
2022-11-16 09:39:11 -0500

    added POST comment route for comment-routes.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const withAuth = require('../../utils/withAuth')
 const { Comment } = require('../../models');
 
-//get all comments.
+//get all comments. route = api/comments/
 router.get('/', (req,res)=>{
     Comment.findAll()
         .then(dbCommentData => res.json(dbCommentData))
@@ -10,6 +10,23 @@ router.get('/', (req,res)=>{
             console.log(err);
             res.status(400).json(err);
         })
+});
+
+//create a comment. route = api/comments/. uses middleware to authenticate.
+router.post('/', withAuth, (req, res)=>{
+    if(req.session){
+        Comment.create({
+            comment_body: req.body.comment_body,
+            user_id: req.session.user_id,
+            post_id: req.session.post_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err=>{
+            console.log(err);
+            res.status(400).json(err);
+        })
+    }
 })
+
 
 module.exports = router;
