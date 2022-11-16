c0738c08d9a60e4c8658d19fcf14397533eb1ade
night <yseddiq@gmail.com>
2022-11-16 09:31:15 -0500

    added get all routes for comments in comment-routes.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,4 +2,14 @@ const router = require('express').Router();
 const withAuth = require('../../utils/withAuth')
 const { Comment } = require('../../models');
 
+//get all comments.
+router.get('/', (req,res)=>{
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err=>{
+            console.log(err);
+            res.status(400).json(err);
+        })
+})
+
 module.exports = router;
